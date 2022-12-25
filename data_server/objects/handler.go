81d5cb4648dadc7ac63d8700a9d8ef7f3be57041
pojiang20 <1021465760@qq.com>
2022-12-25 21:03:35 +0800

    #4 修改dataServer的objects包，删除put接口并给get接口添加校验

--- data_server/objects/handler.go
+++ data_server/objects/handler.go
@@ -7,11 +7,9 @@ import (
 func Handler(w http.ResponseWriter, r *http.Request) {
 	m := r.Method
 
-	if m == http.MethodPut {
-		put(w, r)
-	} else if m == http.MethodGet {
+	if m == http.MethodGet {
 		get(w, r)
-	} else {
-		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
 	}
+	w.WriteHeader(http.StatusMethodNotAllowed)
 }
--- data_server/objects/restful_apis.go
+++ data_server/objects/restful_apis.go
@@ -1,37 +1,47 @@
 package objects
 
 import (
+	"github.com/pojiang20/distribute-object-storage/data_server/locate"
 	"github.com/pojiang20/distribute-object-storage/src/utils"
 	"io"
 	"log"
 	"net/http"
+	"net/url"
 	"os"
+	"path"
 )
 
 const (
 	STORAGE_ROOT = "STORAGE_ROOT"
 )
 
-func put(w http.ResponseWriter, r *http.Request) {
-	f, err := os.Create(os.Getenv(STORAGE_ROOT) + "/objects/" +
-		utils.GetObjectName(r.URL.EscapedPath()))
-	if err != nil {
-		log.Println(err)
-		w.WriteHeader(http.StatusInternalServerError)
+func get(w http.ResponseWriter, r *http.Request) {
+	file := getFile(utils.GetObjectName(r.URL.EscapedPath()))
+	if file == "" {
+		w.WriteHeader(http.StatusNotFound)
 		return
 	}
-	defer f.Close()
-	io.Copy(f, r.Body)
+	sendFile(w, file)
 }
 
-func get(w http.ResponseWriter, r *http.Request) {
-	f, err := os.Open(os.Getenv(STORAGE_ROOT) + "/objects/" +
-		utils.GetObjectName(r.URL.EscapedPath()))
-	if err != nil {
-		log.Println(err)
-		w.WriteHeader(http.StatusInternalServerError)
-		return
+func getFile(hash string) string {
+	file := path.Join(os.Getenv("STORAGE_ROOT"), "objects", hash)
+	f, _ := os.Open(file)
+	d := url.PathEscape(utils.CalculateHash(f))
+	f.Close()
+	// 校验：校验接口层中ES存储的哈希值与实际存储的内容的哈希值是否一致，若是发生了变化则不一致，并且删除该对象数据
+	// 数据存放久了可能会发生数据降解等问题，因此有必要做一致性校验
+	if d != hash {
+		log.Println("object hash mismatch,remove ", file)
+		locate.Add(hash)
+		os.Remove(file)
+		return ""
 	}
+	return file
+}
+
+func sendFile(w io.Writer, file string) {
+	f, _ := os.Open(file)
 	defer f.Close()
 	io.Copy(w, f)
 }
